a26a16381e030a448fa7f77d74c9f2cf6cc078a8
Lisa Okezie <lisa.okezie@gmail.com>
2023-05-07 14:54:39 +0200

    Add repository to manage user entity

---
+++ server/src/main/java/com/tickit/app/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tickit.app.repository;
+
+import com.tickit.app.security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.username = ?1 OR u.email = ?1")
+    User findByEmailOrUsername(String credential);
+}
