ad9ec9f0f6bd49f1b5a5fd1d31d96a9bc0c198d4
Lisa Okezie <lisa.okezie@gmail.com>
2023-05-07 14:54:54 +0200

    Create User entity

---
+++ server/src/main/java/com/tickit/app/security/user/User.java
@@ -0,0 +1,82 @@
+package com.tickit.app.security.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "username_unique",
+                        columnNames = "username"
+                ),
+                @UniqueConstraint(
+                        name = "email_unique",
+                        columnNames = "email"
+                )
+        })
+@Getter
+@Setter
+@NoArgsConstructor
+public class User implements UserDetails, Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String surname;
+    @NotNull
+    private String username;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+    @UpdateTimestamp
+    private LocalDateTime modificationDate;
+
+    @JsonIgnore
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
