232661fe099b491ea938ceea3b02d4e6b7cf9524
laulie1997 <s0568380@htw-berlin.de>
2023-05-22 23:40:32 +0200

    Display user in basic card

--- client/src/app/components/user/user.component.ts
+++ client/src/app/components/user/user.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { TokenStorageService } from '../../services/tokenStorage/token-storage.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
-export class UserComponent {
-
+export class UserComponent implements OnInit {
+  currentUser: any;
+  constructor(private tokenStorage: TokenStorageService) {}
+  ngOnInit() {
+    this.currentUser = this.tokenStorage.getUser();
+  }
 }
