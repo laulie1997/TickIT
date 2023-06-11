19cf3d1e1c54366a5152863d3c76d46216635b3e
laulie1997 <s0568380@htw-berlin.de>
2023-06-11 21:37:15 +0200

    added status service and create, delete, update functions

--- client/src/app/components/status/status.component.ts
+++ client/src/app/components/status/status.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StatusService } from '../../services/status/status.service';
+import { Status } from '../../api/status';
+import { ProjectService } from '../../services/project/project.service';
+import { Project } from '../../api/project';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-status',
   templateUrl: './status.component.html',
-  styleUrls: ['./status.component.css']
+  styleUrls: ['./status.component.css'],
 })
-export class StatusComponent {
+export class StatusComponent implements OnInit {
+  status: Status;
+  form: any = {
+    name: null,
+  };
+  projects: Project[] = [];
+  statuses: Status[] = [];
+  project: Project;
+  constructor(
+    private statusService: StatusService,
+  ) {}
 
+  createStatus() {
+    this.status = this.form;
+    console.log(this.status);
+    this.statusService.createStatus(this.status).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  ngOnInit(): void {}
+
+  deleteStatus() {
+    this.statusService.deleteStatus(this.status).subscribe((response: any) => {
+      console.log('response: ', response);
+    });
+  }
+
+  updateStatus() {
+    this.statusService.updateStatus(this.status).subscribe(response => {
+      this.status = response;
+    });
+  }
 }
