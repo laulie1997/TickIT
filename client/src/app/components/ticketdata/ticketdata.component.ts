f5581e003c7ce17debb83adea4830113426ec03e
laulie1997 <s0568380@htw-berlin.de>
2023-06-12 08:13:18 +0200

    createTicket function with 401 error

--- client/src/app/components/ticketdata/ticketdata.component.ts
+++ client/src/app/components/ticketdata/ticketdata.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Ticket } from '../../api/ticket';
+import { TicketService } from '../../services/ticket/ticket.service';
 
 @Component({
   selector: 'app-ticketdata',
@@ -8,12 +9,32 @@ import { Ticket } from '../../api/ticket';
   styleUrls: ['./ticketdata.component.css'],
 })
 export class TicketdataComponent {
+  form: any = {
+    title: null,
+    description: null,
+    dueDate: null,
+  };
+  errorMessage: '';
   constructor(
     public dialogRef: MatDialogRef<Ticket>,
+    private ticketService: TicketService,
     @Inject(MAT_DIALOG_DATA) public ticket: Ticket
   ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  createTicket() {
+    this.ticket = this.form;
+    console.log(this.ticket);
+    this.ticketService.createTicket(this.ticket).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        this.errorMessage = error.error.message;
+      }
+    );
   }
 }
--- client/src/app/services/ticket/ticket.service.ts
+++ client/src/app/services/ticket/ticket.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Ticket } from '../../api/ticket';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TicketService {
-  baseURL: string = '/api/v1/ticket/';
-  constructor() {}
+  baseURL: string = '/api/v1/ticket';
+  constructor(private http: HttpClient) {}
+
+  createTicket(ticket: Ticket): Observable<Ticket> {
+    return this.http.post<Ticket>(this.baseURL, ticket);
+  }
 }
