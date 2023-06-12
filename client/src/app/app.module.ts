a8d9e9f767662fb25c16d6c01466f37b160042cb
laulie1997 <s0568380@htw-berlin.de>
2023-06-12 06:48:59 +0200

    Added Ticket Service and Component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { ProjectComponent } from './components/project/project.component';
 import { ProjectBoardComponent } from './components/project-board/project-board.component';
 import { httpInterceptorProviders } from './helpers/http.interceptor';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { TicketComponent } from './components/ticket/ticket.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { ProjectsComponent } from './components/projects/projects.component';
     ProjectComponent,
     ProjectBoardComponent,
     ProjectsComponent,
+    TicketComponent,
   ],
   imports: [
     BrowserModule,
