450824cfab4adb0c9f158a853207f2803db691fc
laulie1997 <s0568380@htw-berlin.de>
2023-06-12 12:40:32 +0200

    added Ticket preview for board

--- client/src/app/components/ticketsmall/ticketsmall.component.ts
+++ client/src/app/components/ticketsmall/ticketsmall.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Ticket } from '../../api/ticket';
+import { TicketdataComponent } from '../ticketdata/ticketdata.component';
 
 @Component({
   selector: 'app-ticketsmall',
   templateUrl: './ticketsmall.component.html',
-  styleUrls: ['./ticketsmall.component.css']
+  styleUrls: ['./ticketsmall.component.css'],
 })
 export class TicketsmallComponent {
+  ticket: Ticket;
+  constructor(public dialog: MatDialog) {}
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TicketdataComponent, {
+      width: '500px',
+      data: {},
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.ticket = result;
+    });
+  }
 }
