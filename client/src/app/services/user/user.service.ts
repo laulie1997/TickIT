8ef9f3c0b027cc822a584b6fdb35095ddf43f46d
Lisa Okezie <lisa.okezie@gmail.com>
2023-06-14 21:09:09 +0200

    Add endpoint to retrieve all users

--- client/src/app/services/user/user.service.ts
+++ client/src/app/services/user/user.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
+import { EMPTY, Observable, map } from 'rxjs';
 import { User } from '../../api/user';
 import { HttpClient } from '@angular/common/http';
 import { PasswordChangeRequest } from '../../api/passwordChangeRequest';
+import { UserWrapper } from 'src/app/api/userWrapper';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   private readonly baseUrl: string = '/api/v1/security/user';
+
   constructor(private httpClient: HttpClient) {}
 
+  getUsers(): Observable<User[]> {
+    return this.httpClient
+      .get<UserWrapper>(this.baseUrl)
+      .pipe(map((wrapper: UserWrapper) => wrapper.users));
+  }
+
   getUser(userId: number): Observable<User> {
-    console.log(this.baseUrl + '/' + userId);
     return this.httpClient.get<User>(this.baseUrl + '/' + userId);
   }
 
