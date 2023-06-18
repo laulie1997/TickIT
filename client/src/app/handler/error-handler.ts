b5ba012efa4ce13a5d352b1137d9fed126692d27
laulie1997 <s0568380@htw-berlin.de>
2023-06-18 17:48:02 +0200

    401 and 403 errors added

--- client/src/app/handler/error-handler.ts
+++ client/src/app/handler/error-handler.ts
@@ -4,19 +4,33 @@ import { ErrorService } from '../services/error/error.service';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
-  constructor(private errorService: ErrorService) {}
+  constructor(private errorService: ErrorService) {
+  }
+
   handleError(error: any): void {
     this.errorService.showErrorMessage('Es ist ein Fehler aufgetreten');
     if (error instanceof HttpErrorResponse) {
       if (error.status === 404) {
-        this.errorService.showErrorMessage('Die Seite konnte nicht gefunden werden');
+        this.errorService.showErrorMessage(
+          'Die Seite konnte nicht gefunden werden'
+        );
       } else if (error.status === 500) {
         this.errorService.showErrorMessage('Interner Server Fehler');
+      } else if (error.status === 401) {
+        this.errorService.showErrorMessage(
+          'Bitte authentifizieren Sie sich, um diese Aktion auszuführen');
+      } else if (error.status === 403) {
+        this.errorService.showErrorMessage(
+          'Sie sind nicht berechtigt diese Aktion auszuführen');
+      } else if (error instanceof TypeError) {
+        console.log('Type Error:', error);
+      } else {
+        this.errorService.showErrorMessage(
+          'Es ist ein unbekannter Fehler aufgetreten'
+        );
       }
-    } else if (error instanceof TypeError) {
-      console.log('Type Error:', error);
-    } else {
-      this.errorService.showErrorMessage('Es ist ein unbekannter Fehler aufgetreten');
     }
   }
 }
+
+
