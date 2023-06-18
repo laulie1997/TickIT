c85bf912e28e0010bfdd372607fbe519110d0849
laulie1997 <s0568380@htw-berlin.de>
2023-06-18 17:34:33 +0200

    Functions added

--- client/src/app/handler/error-handler.ts
+++ client/src/app/handler/error-handler.ts
@@ -1,7 +1,20 @@
 import { ErrorHandler, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
   handleError(error: any): void {
+    console.error('An error occurred:', error);
+    if (error instanceof HttpErrorResponse) {
+      if (error.status === 404) {
+        console.error('Resource not found:', error.url);
+      } else if (error.status === 500) {
+        console.error('Internal Server Error:', error.message);
+      }
+    } else if (error instanceof TypeError) {
+      console.error('Type Error:', error.message);
+    } else {
+      console.error('Unknown Error:', error);
+    }
   }
 }
