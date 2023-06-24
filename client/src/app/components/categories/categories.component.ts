6d519c5e74d6db3bcf415db6cbc393c52e8f2d90
laulie1997 <s0568380@htw-berlin.de>
2023-06-24 12:06:46 +0200

    moved fetchCategories to categories component

--- client/src/app/components/categories/categories.component.ts
+++ client/src/app/components/categories/categories.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Category} from "../../api/category";
+import {CategoryService} from "../../services/category/category.service";
+import {CategoriesModalComponent} from "../categories-modal/categories-modal.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit{
+  categories: Category[] = [];
+  constructor(private categoryService: CategoryService,
+              private dialog: MatDialog,) {
+  }
 
+  ngOnInit(): void {
+  }
+
+  fetchCategories(projectId: any) {
+    this.categoryService
+      .getCategories(projectId)
+      .subscribe((categories: Category[]) => {
+        this.categories = categories;
+        console.log(this.categories);
+      });
+  }
+
+  openEditCategoryModal(): void {
+    const dialogRef = this.dialog.open(CategoriesModalComponent, {
+      width: '500px',
+      data: {},
+    });
+  }
 }
