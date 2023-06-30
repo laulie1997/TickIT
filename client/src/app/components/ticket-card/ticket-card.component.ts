e1444376cf1132579fc4503ccea9ca6d47c6d10f
laulie1997 <s0568380@htw-berlin.de>
2023-06-30 23:12:33 +0200

    checks for ticket on dashboard, output if there are no categories in a project

--- client/src/app/components/ticket-card/ticket-card.component.ts
+++ client/src/app/components/ticket-card/ticket-card.component.ts
@@ -15,7 +15,11 @@ export class TicketCardComponent implements OnInit {
   @Input() ticket: Ticket;
   @Input() project: Project;
   @Input() projectId: number;
-  constructor(public dialog: MatDialog, private ticketService: TicketService) {}
+  constructor(
+    public dialog: MatDialog,
+    private ticketService: TicketService,
+    public router: Router
+  ) {}
 
   openDialog(): void {
     const dialogRef = this.dialog.open(TicketModalComponent, {
